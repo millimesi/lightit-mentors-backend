1def92beefbf53ced511e8f28446779cb9ae833d
millimesi <millionmesi1@gmail.com>
2025-05-28 12:16:30 +0300

    fix(mentorRequest): Correct typos in schema documentation and update property names to new model schema

--- models/mentorRequest.js
+++ models/mentorRequest.js
@@ -1,36 +1,44 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
 /**
- * @description mentorRequest schema for mentor to list or requess
+ * @description mentorRequest schema for mentor to list or request
  * @property {String} userId - id of the requesting user
  * @property {String} mentorId - id of the requested mentor
  * @property {String} menteeFullName - information of the mentee
- * @property {String} location - location of the mentorship or online
- * @property {String} Status - status of the reques enum of
- * 'pending', 'Accepted', 'Regjected'
+ * @property {String} residentialArea - Residential area/neighborhood of the mentee
+ * @property {String} Status - status of the request enum of
+ * 'pending', 'Accepted', 'Rejected'
  * @property {String} message - message for the mentor
  */
 const mentorRequestSchema = new Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    mentorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Mentor', required: true },
-    menteeFullName: { type: String, required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    mentorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Mentor",
+      required: true,
+    },
+    numberOfMentees: { type: Number, required: true, default: 1 },
     message: { type: String },
-    location: { type: String },
+    residentialArea: { type: String },
     status: {
       type: String,
       enum: {
-        values: ['pending', 'accepted', 'rejected', 'Declined'],
-        message: '{VALUE} is not supported',
+        values: ["pending", "accepted", "rejected", "Declined"],
+        message: "{VALUE} is not supported",
       },
-      default: 'pending'
+      default: "pending",
     },
   },
   { timestamps: true }, // Automatically adds createdAt and updatedAt time stamp
-  { versionKey: '__v' }, // Automatical creates and update version of the schema
+  { versionKey: "__v" } // Automatically creates and update version of the schema
 );
 
-const MentorRequest = mongoose.model('MentorRequest', mentorRequestSchema);
+const MentorRequest = mongoose.model("MentorRequest", mentorRequestSchema);
 export default MentorRequest;

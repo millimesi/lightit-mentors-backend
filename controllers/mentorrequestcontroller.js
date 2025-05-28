9bd3c99fa1600c7bcc03b84b9fa529325e50cb2b
millimesi <millionmesi1@gmail.com>
2025-05-28 12:15:27 +0300

    fix(MentorRequestController): Update the request data, request validation and error messages to meet the its database model

--- controllers/mentorrequestcontroller.js
+++ controllers/mentorrequestcontroller.js
@@ -17,39 +17,39 @@ export default class MentorRequestController {
     // get the request data
     const mentorId = req.body.mentorId || null;
     const userId = req.body.userId || null;
-    const menteeFullName = req.body.menteeFullName || null;
-    const location = req.body.location || null;
+    const numberOfMentees = parseInt(req.body.numberOfMentees) || null;
+    const residentialArea = req.body.residentialArea || null;
     const message = req.body.message || null;
 
     // check if all required request data exists
-    if (!(mentorId && userId && menteeFullName && location)) {
+    if (!(mentorId && userId && numberOfMentees && residentialArea)) {
       // return incomplete data
-      return res.status(400).json({ error: "incomplete request data" });
+      return res.status(400).json({ error: "Incomplete request data" });
     }
 
-    // check the existance of the user and mentor
+    // check the existence of the user and mentor
     try {
       // check if the mentor exists by the id
       const mentorRequested = await Mentor.findOne({ _id: mentorId }); // returns true if it exists
 
       // respond mentor id doesnt exist
       if (!mentorRequested) {
-        return res.status(400).json({ error: "mentor doesnt exist" });
+        return res.status(400).json({ error: "Mentor doesn't exist" });
       }
 
       // chek if the user exists by the id
       const userExists = await User.exists({ _id: userId });
 
       if (!userExists) {
-        return res.status(400).json({ error: "user doesn't exist" });
+        return res.status(400).json({ error: "User doesn't exist" });
       }
 
-      // create mentorrequest object
+      // create mentor request object
       const mentorRequestObj = {
         mentorId,
         userId,
-        menteeFullName,
-        location,
+        numberOfMentees,
+        residentialArea,
         message,
       };
 
@@ -63,8 +63,8 @@ export default class MentorRequestController {
       const emailhtmlBody = `
                 <html>
                 <body>
-                    <p><b>You have a new mentor request for a user mentee ${menteeFullName}.</b></h3>
-                    <p><strong>Location:</strong> ${location}</p>
+                    <p><b>You have a new mentor request for ${numberOfMentees} mentees.</b></h3>
+                    <p><strong>Location:</strong> ${residentialArea}</p>
                     <p><strong>Message:</strong> ${message}</p>
                     <p>
                         <a href="http://localhost:5000/mentor_request/${insertInfo._id}/accepted" style="padding: 10px 20px; background-color: #28a745; color: #fff; text-decoration: none; border-radius: 5px;">Accept Request</a>
@@ -73,12 +73,14 @@ export default class MentorRequestController {
                 </body>
                 </html>
             `;
-
-      await addEmailJob(mentorEmail, emailSubject, emailhtmlBody);
+      // 💥This Job is not working needs fix
+      // await addEmailJob(mentorEmail, emailSubject, emailhtmlBody);
 
       // if the request is not proccessed with the given time it will be declined by light it
       console.log(insertInfo._id);
-      await addDeclineRequestJob(insertInfo._id);
+
+      // 💥This is not working Please Check it
+      // await addDeclineRequestJob(insertInfo._id);
 
       // respond with request id and request status
       return res.status(200).json({
@@ -132,7 +134,7 @@ export default class MentorRequestController {
 
       // if the status is not  one of them respond the request is processed
       if (requestStatus !== "pending") {
-        return res.status(409).json({ messege: `request is ${requestStatus}` });
+        return res.status(409).json({ message: `request is ${requestStatus}` });
       }
 
       // update the status of the mentor request
@@ -154,18 +156,18 @@ export default class MentorRequestController {
         const mentorId = updatedRequest.mentorId;
         const mentorRequested = await Mentor.findOne({ _id: mentorId });
 
-        const userEmail = requestingUser.email;
-        const emailSubject = "Your request for mentor is rejected!";
-        const emailhtmlBody = `
-                    <html>
-                    <body>
-                        <p> Dear ${requestingUser.name} your request to Mentor ${mentorRequested.name} is <b>rejected</b>.</p>
-                        <p><strong>For furthe assistance contact light it Mentors!</strong></p>
-                    </body>
-                    </html>
-                `;
-
-        await addEmailJob(userEmail, emailSubject, emailhtmlBody);
+        // const userEmail = requestingUser.email;
+        // const emailSubject = "Your request for mentor is rejected!";
+        // const emailhtmlBody = `
+        //             <html>
+        //             <body>
+        //                 <p> Dear ${requestingUser.name} your request to Mentor ${mentorRequested.name} is <b>rejected</b>.</p>
+        //                 <p><strong>For furthe assistance contact light it Mentors!</strong></p>
+        //             </body>
+        //             </html>
+        //         `;
+        // 💥This Job is not working needs fix
+        // await addEmailJob(userEmail, emailSubject, emailhtmlBody);
 
         return res.status(200).json({ messege: "request rejected" });
       }
@@ -195,45 +197,47 @@ export default class MentorRequestController {
       });
 
       // get the mentor email and send him email giving detail for the contact of user
-      const mentorRequested = await Mentor.findOne({ _id: mentorId });
+      // const mentorRequested = await Mentor.findOne({ _id: mentorId });
 
       // send an email for the requested mentor
-      const mentorEmail = mentorRequested.email;
-      const emailSubject = "You can find details of the request here!";
-      const emailhtmlBody = `
-                <html>
-                <body>
-                    <p> Dear Mentor contact the user and start mentoring ${updatedRequest.menteeFullName}.</h3>
-                    <p><strong>Phone Number:</strong> ${requestingUser.phoneNumber}</p>
-                    <p><strong>Email:</strong> ${requestingUser.email}</p>
-                    <p><strong>Location:</strong> ${updatedRequest.location}</p>
-                    <p><strong>Message:</strong> ${updatedRequest.message}</p>
-                    <p style="padding: 10px 20px; background-color: #28a745; color: #fff; text-decoration: none; border-radius: 5px; font-weight: bold; font-size: 20px; display: inline-block;">
-                        You are Trusted for Brightening the Future!
-                    </p>
+      // const mentorEmail = mentorRequested.email;
+      // const emailSubject = "You can find details of the request here!";
+      // const emailhtmlBody = `
+      //           <html>
+      //           <body>
+      //               <p> Dear Mentor contact the user and start mentoring ${updatedRequest.menteeFullName}.</h3>
+      //               <p><strong>Phone Number:</strong> ${requestingUser.phoneNumber}</p>
+      //               <p><strong>Email:</strong> ${requestingUser.email}</p>
+      //               <p><strong>Location:</strong> ${updatedRequest.location}</p>
+      //               <p><strong>Message:</strong> ${updatedRequest.message}</p>
+      //               <p style="padding: 10px 20px; background-color: #28a745; color: #fff; text-decoration: none; border-radius: 5px; font-weight: bold; font-size: 20px; display: inline-block;">
+      //                   You are Trusted for Brightening the Future!
+      //               </p>
 
-                </body>
-                </html>
-            `;
+      //           </body>
+      //           </html>
+      //       `;
 
-      await addEmailJob(mentorEmail, emailSubject, emailhtmlBody);
+      // 💥This Job is not working needs fix
+      // await addEmailJob(mentorEmail, emailSubject, emailhtmlBody);
 
+      // 💥This Job is not working needs fix
       // send email for the user to notify the request is accepted!
-      const userEmail = requestingUser.email;
-      const userEmailSubject = "Your request for mentor is Accepted!";
-      const userEmailhtmlBody = `
-                    <html>
-                    <body>
-                        <p> Dear ${requestingUser.name} your request to Mentor ${mentorRequested.name} is <b>accepted</b>.</p>
-                        <p>contact the mentor:</p>
-                        <p><strong>Phone Number:</strong> ${mentorRequested.phoneNumber}</p>
-                        <p><strong>Email:</strong> ${mentorRequested.email}</p>
-                        <p><strong>Reach us for further assistance!</strong></p>
-                    </body>
-                    </html>
-                `;
+      // const userEmail = requestingUser.email;
+      // const userEmailSubject = "Your request for mentor is Accepted!";
+      // const userEmailhtmlBody = `
+      //               <html>
+      //               <body>
+      //                   <p> Dear ${requestingUser.name} your request to Mentor ${mentorRequested.name} is <b>accepted</b>.</p>
+      //                   <p>contact the mentor:</p>
+      //                   <p><strong>Phone Number:</strong> ${mentorRequested.phoneNumber}</p>
+      //                   <p><strong>Email:</strong> ${mentorRequested.email}</p>
+      //                   <p><strong>Reach us for further assistance!</strong></p>
+      //               </body>
+      //               </html>
+      //           `;
 
-      await addEmailJob(userEmail, userEmailSubject, userEmailhtmlBody);
+      // await addEmailJob(userEmail, userEmailSubject, userEmailhtmlBody);
     } catch (err) {
       console.log(`Error: ${err}`);
       res.status(500).json({ error: "Server error occurred" });

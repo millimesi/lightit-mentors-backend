5fea8d37190c29549a58844b92d07dc8ab28cf03
millimesi <millionmesi1@gmail.com>
2025-05-31 16:47:52 +0300

    refactor(server): add error handler middleware and improve code comments

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ import config from "./config/config.js";
 import usersRouter from "./routes/users.js";
 import mentorsRouter from "./routes/mentors.js";
 import mentorRequestRouter from "./routes/mentorRequest.js";
+import errorHandler from "./middlewares/errorMiddleware.js";
 
 const api = express();
 
-// add request body json parser
+// Add request body json parser
 api.use(express.json());
 
-// serve static images from profileImage
+// Serve static images from profileImage
 api.use("/profileImage", express.static("public/profileImage"));
 
-// insert the routes
+// Insert the routes
 api.use("/users", usersRouter);
 api.use("/mentors", mentorsRouter);
 api.use("/mentor_request", mentorRequestRouter);
+
+// Error handler
+api.use(errorHandler);
 
 api.listen(config.port, () => {
   console.log(`✅ API server is listening on port: ${config.port}`);

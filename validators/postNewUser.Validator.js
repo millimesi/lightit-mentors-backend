f7f5785f8f7bfae99bd49fe5e803d85c90ccea22
millimesi <millionmesi1@gmail.com>
2025-06-02 15:34:13 +0300

    refactor(validators): update postNewUserValidators to use exists() for required fields

--- validators/postNewUser.Validator.js
+++ validators/postNewUser.Validator.js
@@ -2,17 +2,27 @@ import { body } from "express-validator";
 
 const postNewUserValidators = [
   body("name")
-    .notEmpty()
-    .withMessage("Name is Required")
+    .exists({ checkFalsy: true })
+    .withMessage("Name is required")
+    .bail()
     .isAlpha()
     .withMessage("Name can not have numbers"),
   body("fatherName")
-    .notEmpty()
-    .withMessage("Father Name is Required")
+    .exists({ checkFalsy: true })
+    .withMessage("Father Name is required")
+    .bail()
     .isAlpha()
     .withMessage("Father Name can not have numbers"),
-  body("email").isEmail().withMessage("Must be valid email format"),
+  body("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Email is required")
+    .bail()
+    .isEmail()
+    .withMessage("Must be valid email format"),
   body("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Password is required")
+    .bail()
     .isLength({ min: 6 })
     .withMessage("Password must at least have 6 characters"),
 ];

fb4548b9109e818c8a899c105886a7928b229e61
millimesi <millionmesi1@gmail.com>
2025-06-09 11:07:55 +0300

    fix(authenticateToken): streamline error handling by using AppError for authorization failures

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import AppError from "../utils/appError.js";
 
 dotenv.config();
 
@@ -8,12 +9,10 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token)
-    return res.status(401).json({ error: "Access needs authorization" });
-
+  if (!token) return next(new AppError("Access needs authorization", 401));
   // Verify the token
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: " Access unauthorized" });
+    if (err) return next(new AppError("Access unauthorized", 403));
 
     req.user = user; // Attach the decoded token payload to the request object
     next(); // Pass control to the next middleware function

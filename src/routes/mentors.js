a576c498f3f4e15f831bbb325ec65f5e903571e2
millimesi <millionmesi1@gmail.com>
2025-06-09 11:08:17 +0300

    fix(mentors): disable authentication for mentor list retrieval

--- src/routes/mentors.js
+++ src/routes/mentors.js
@@ -5,7 +5,8 @@ import authenticateToken from "../middlewares/authenticateToken.js";
 const router = express.Router();
 
 // Retrieves list of paginated mentors
-router.get("/", authenticateToken, MentorController.getMentors);
+// router.get("/", authenticateToken, MentorController.getMentors);
+router.get("/", MentorController.getMentors);
 
 //
 // Retrieves mentor of the specified Id
